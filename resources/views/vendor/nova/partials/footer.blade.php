439cf7b09d3cc21cbf0fd93de75c3c1ac9c806d7
Wasan Ongart <o_wasan@hotmail.com>
2021-01-30 06:39:49 +0700

    update footer

--- resources/views/vendor/nova/partials/footer.blade.php
+++ resources/views/vendor/nova/partials/footer.blade.php
@@ -1,5 +1,5 @@
 <p class="mt-8 text-center text-xs text-80">
-    <a href="https://app.sisahygo.online" class="text-primary dim no-underline">SISAHYGO</a>
+    <a href="https://app.sisahygo.online" target ="_blank" class="text-primary dim no-underline">SISAHYGO</a>
     <span class="px-1">&middot;</span>
     &copy; {{ date('Y') }} Sisahy Transport(1988) Co.,Ltd. : - By Wasandev
     <span class="px-1">&middot;</span>
