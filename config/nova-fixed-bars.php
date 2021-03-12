5cf7768076fbf11aefd0608be6a92755554cf635
Wasan Ongart <o_wasan@hotmail.com>
2021-03-12 13:49:56 +0700

    update document and fixed-bar

--- config/nova-fixed-bars.php
+++ config/nova-fixed-bars.php
@@ -24,6 +24,6 @@ return [
   |
   */
 
-    'fixedNavbar' => true,
+    'fixedNavbar' => false,
 
 ];
--- config/nova-theme-responsive.php
+++ config/nova-theme-responsive.php
@@ -15,10 +15,10 @@ return [
     'resource_tables_sticky_actions_on_mobile' => false,
 
     // If true, hides the "Update & Continue Editing" button on "Update" forms
-    'hide_update_and_continue_editing_button' => false,
+    'hide_update_and_continue_editing_button' => true,
 
     // If true, hides the "Update & Continue Editing" button on "Update" forms (mobile only)
-    'hide_update_and_continue_editing_button_on_mobile' => false,
+    'hide_update_and_continue_editing_button_on_mobile' => true,
 
     // If true, the sidebar will stay fixed on desktop view
     'fixed_sidebar' => false,
