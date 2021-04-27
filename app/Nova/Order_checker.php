f94fe67f46950971592126abcf29238c3e2e8300
Wasan Ongart <o_wasan@hotmail.com>
2021-04-27 16:28:28 +0700

    qrcode scan

--- app/Nova/Order_checker.php
+++ app/Nova/Order_checker.php
@@ -22,6 +22,7 @@ use Wasandev\Orderstatus\Orderstatus;
 use Epartment\NovaDependencyContainer\HasDependencies;
 use Epartment\NovaDependencyContainer\NovaDependencyContainer;
 use Laravel\Nova\Http\Requests\ActionRequest;
+use phpDocumentor\Reflection\PseudoTypes\True_;
 use Tsungsoft\QrCodeReader\QrCodeReader;
 
 class Order_checker extends Resource
@@ -107,9 +108,9 @@ class Order_checker extends Resource
                 ->default('general')
                 ->displayUsingLabels(),
             QrCodeReader::make('scan Qr code ผู้ส่ง(ถ้ามี)', 'customer_id')   // Name -> label name, name_id -> save to column
-                ->canInput()                        // the user able to input the code using keyboard, default false
-                ->canSubmit()                       // on modal scan need to click submit to send the code to the input value, default false
-                ->displayValue()                    // set qr size on detail, default 100
+                ->canInput('true')                        // the user able to input the code using keyboard, default false
+                ->canSubmit('true')                       // on modal scan need to click submit to send the code to the input value, default false
+                ->displayValue('true')                    // set qr size on detail, default 100
                 ->qrSizeIndex()                     // set qr size on index, default 30
                 ->qrSizeDetail()                    // set qr size on detail, default 100
                 ->qrSizeForm()                      // set qr size on form, default 50
