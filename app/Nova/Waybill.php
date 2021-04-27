e094de0030e2b57ecb07a1e05d431f6f14af5453
Wasan Ongart <o_wasan@hotmail.com>
2021-04-27 15:19:57 +0700

    ีupdate waybill

--- app/Nova/Waybill.php
+++ app/Nova/Waybill.php
@@ -100,7 +100,7 @@ class Waybill extends Resource
     public function fields(Request $request)
     {
         return [
-            //ID::make(__('ID'), 'id')->sortable(),
+            ID::make(__('ID'), 'id')->sortable(),
             Status::make(__('Status'), 'waybill_status')
                 ->loadingWhen(['loading'])
                 ->failedWhen(['cancel'])
