f906f11640f6d8a5f92cc20d9e2fd57753024b70
Wasan Ongart <o_wasan@hotmail.com>
2021-07-13 11:11:41 +0700

    ีupdate orderconfirmed

--- app/Nova/Actions/OrderConfirmed.php
+++ app/Nova/Actions/OrderConfirmed.php
@@ -5,6 +5,9 @@ namespace App\Nova\Actions;
 use App\Models\Bankaccount;
 use App\Models\Order_header;
 use App\Models\Order_detail;
+use App\Models\Routeto_branch;
+use App\Models\User;
+use App\Models\Waybill;
 use Illuminate\Bus\Queueable;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Queue\InteractsWithQueue;
@@ -65,6 +68,8 @@ class OrderConfirmed extends Action
                 $model->paymenttype =  $fields->paymenttype;
                 $model->bankaccount_id = $fields->bankaccount;
                 $model->bankreference = $fields->refernce;
+                $model->waybill_id = $fields->waybill_branch;
+                $model->loader_id = $fields->loader;
                 $model->order_status = 'confirmed';
 
                 $model->save();
@@ -85,6 +90,9 @@ class OrderConfirmed extends Action
     public function fields()
     {
         $bankaccount = Bankaccount::where('defaultflag', '=', true)->pluck('account_no', 'id');
+        $waybills = Waybill::where('waybill_status', 'loading')->pluck('waybill_no', 'id');
+        $loaders = User::where('branch_id', '=', auth()->user()->branch_id);
+        $waybillOptions = [];
 
         if ($this->model) {
             $order_amount = 0;
@@ -97,26 +105,51 @@ class OrderConfirmed extends Action
                 $order_amount = $order_amount + $sub_total;
             }
 
+            $routeto_branch = Routeto_branch::where('dest_branch_id',  $order_header->branch_rec_id)->first();
 
+            $waybills = Waybill::with('car')
+                ->where('routeto_branch_id', '=', $routeto_branch->id)
+                ->where('waybill_status', '=', 'loading')
+                ->get();
+            if (isset($waybills)) {
+                foreach ($waybills as $waybill) {
+                    $waybillOptions[] = [
+                        ['branchwaybill' => ['id' => $waybill->id, 'name' => $waybill->waybill_no . '-' . $waybill->car->car_regist]],
+                    ];
+                }
+                $selectOptions = collect($waybillOptions)->flatten(1);
 
-            return [
-                Currency::make('ค่าขนส่ง', 'order_amount')->default($order_amount)
-                    ->readonly(),
-                Select::make(__('Payment type'), 'paymenttype')->options([
-                    'H' => 'เงินสดต้นทาง',
-                    'T' => 'เงินโอนต้นทาง',
-                    'E' => 'เก็บเงินปลายทาง',
-                    'F' => 'วางบิลต้นทาง',
-                    'L' => 'วางบิลปลายทาง'
-                ])->displayUsingLabels()
-                    ->default($paymenttype),
-                NovaDependencyContainer::make([
-                    Select::make(__('Bank Account no'), 'bankaccount')
-                        ->options($bankaccount)
-                        ->displayUsingLabels(),
-                    Text::make(__('Bank reference no'), 'reference'),
-                ])->dependsOn('paymenttype', 'T'),
-            ];
+                $waybillOptions = $selectOptions->pluck('branchwaybill.name', 'branchwaybill.id');
+            }
+            if (isset($waybillOptions)) {
+
+                return [
+                    Currency::make('ค่าขนส่ง', 'order_amount')->default($order_amount)
+                        ->readonly(),
+                    Select::make(__('Payment type'), 'paymenttype')->options([
+                        'H' => 'เงินสดต้นทาง',
+                        'T' => 'เงินโอนต้นทาง',
+                        'E' => 'เก็บเงินปลายทาง',
+                        'F' => 'วางบิลต้นทาง',
+                        'L' => 'วางบิลปลายทาง'
+                    ])->displayUsingLabels()
+                        ->default($paymenttype),
+                    NovaDependencyContainer::make([
+                        Select::make(__('Bank Account no'), 'bankaccount')
+                            ->options($bankaccount)
+                            ->displayUsingLabels(),
+                        Text::make(__('Bank reference no'), 'reference'),
+                    ])->dependsOn('paymenttype', 'T'),
+                    Select::make('สินค้าขึ้นรถแล้ว เลือกใบกำกับ', 'waybill_branch')
+                        ->options($waybillOptions)
+                        ->displayUsingLabels()
+                        ->searchable(),
+                    Select::make('พนักงานจัดขึ้น', 'loader')
+                        ->options($loaders)
+                        ->displayUsingLabels()
+                        ->searchable(),
+                ];
+            }
         }
         return [
             Select::make(__('Payment type'), 'paymenttype')->options([
@@ -135,6 +168,14 @@ class OrderConfirmed extends Action
                     Text::make(__('Bank reference no'), 'reference'),
                 ]
             )->dependsOn('paymenttype', 'T'),
+            Select::make(__('Waybill'), 'waybill_branch')
+                ->options($waybills)
+                ->displayUsingLabels()
+                ->searchable(),
+            Select::make('พนักงานจัดขึ้น', 'loader')
+                ->options($loaders)
+                ->displayUsingLabels()
+                ->searchable(),
         ];
     }
 }
