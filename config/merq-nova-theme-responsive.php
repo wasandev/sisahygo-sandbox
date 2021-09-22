e6a1feaeb5a5dc45eaa3b18652bd0c1add1b90aa
Wasan Ongart <o_wasan@hotmail.com>
2021-09-22 11:34:03 +0700

    update nova response modal

--- config/merq-nova-theme-responsive.php
+++ config/merq-nova-theme-responsive.php
@@ -9,13 +9,13 @@ return [
     'hidden_sidebar_headlines' => [],
 
     // If true, the resource tables actions will be always visible (sticky)
-    'resource_tables_sticky_actions' => false,
+    'resource_tables_sticky_actions' => true,
 
     // If true, the resource tables actions will be always visible (sticky) on mobile
-    'resource_tables_sticky_actions_on_mobile' => false,
+    'resource_tables_sticky_actions_on_mobile' => true,
 
     // If true, hides the "Update & Continue Editing" button on "Update" forms
-    'hide_update_and_continue_editing_button' => false,
+    'hide_update_and_continue_editing_button' => true,
 
     // If true, hides the "Update & Continue Editing" button on "Update" forms (mobile only)
     'hide_update_and_continue_editing_button_on_mobile' => true,
