60c1c0c8b8ae93f75c6c501ffb6d130af5e78a7f
Wasan Ongart <o_wasan@hotmail.com>
2021-09-22 11:00:56 +0700

    update model for customer create

--- app/Nova/Customer.php
+++ app/Nova/Customer.php
@@ -97,24 +97,34 @@ class Customer extends Resource
                 ->detailSize(200)
                 ->onlyOnDetail(),
 
-            Boolean::make(__('Status'), 'status')->hideFromIndex(),
+            Boolean::make(__('Status'), 'status')
+                ->hideFromIndex()
+                ->default(true)
+                ->hideWhenCreating(),
             Text::make(__('Customer code'), 'customer_code')
                 ->readonly()
-                ->hideFromIndex(),
+                ->hideFromIndex()
+                ->hideWhenCreating(),
 
             Text::make(__('Name'), 'name')
                 ->sortable()
                 ->rules('required', 'max:250'),
 
             Text::make(__('Tax ID'), 'taxid')
-                ->hideFromIndex(),
+                ->hideFromIndex()
+                ->canSee(function ($request) {
+                    return $request->user()->hasPermissionTo('manage order_headers');
+                }),
             //->rules('digits:13', 'numeric'),
             Select::make(__('Type'), 'type')->options([
                 'company' => 'นิติบุคคล',
                 'person' => 'บุคคลธรรมดา'
             ])
                 ->displayUsingLabels()
-                ->hideFromIndex(),
+                ->hideFromIndex()
+                ->canSee(function ($request) {
+                    return $request->user()->hasPermissionTo('manage order_headers');
+                }),
 
             Select::make(__('Payment type'), 'paymenttype')->options([
                 'H' => 'เงินสดต้นทาง',
@@ -123,14 +133,23 @@ class Customer extends Resource
             ])
                 ->hideFromIndex()
                 ->withMeta(['value' => 'H'])
-                ->displayUsingLabels(),
+                ->displayUsingLabels()
+                ->canSee(function ($request) {
+                    return $request->user()->hasPermissionTo('manage order_headers');
+                }),
             Number::make(__('Credit term'), 'creditterm')
                 ->withMeta(['value' => 0])
-                ->hideFromIndex(),
+                ->hideFromIndex()
+                ->canSee(function ($request) {
+                    return $request->user()->hasPermissionTo('manage order_headers');
+                }),
             BelongsTo::make(__('Business type'), 'businesstype', 'App\Nova\Businesstype')
                 ->showCreateRelationButton()
                 ->sortable()
-                ->nullable(),
+                ->nullable()
+                ->canSee(function ($request) {
+                    return $request->user()->hasPermissionTo('manage order_headers');
+                }),
 
             BelongsTo::make(__('Created by'), 'user', 'App\Nova\User')
                 ->onlyOnDetail(),
@@ -143,56 +162,37 @@ class Customer extends Resource
                 ->format('DD/MM/YYYY HH:mm')
                 ->onlyOnDetail(),
 
-            new Panel('ข้อมูลการติดต่อ', $this->contactFields()),
-            new Panel('ที่อยู่ในการออกเอกสาร', $this->addressFields()),
-            new Panel('อื่นๆ', $this->otherFields()),
-            HasMany::make(__('Customer addresses'), 'addresses', 'App\Nova\Address'),
-            //BelongsToMany::make(__('Customer products'), 'product', 'App\Nova\Product'),
-            //HasMany::make(__('Customer shipping cost'), 'customer_product_prices', 'App\Nova\Customer_product_price'),
-            HasOne::make(__('Assign user'), 'assign_customer', 'App\Nova\User')
-                ->canSee(function ($request) {
-                    return $request->user()->role == 'admin';
-                }),
-            HasMany::make('รายการส่งสินค้า', 'order_sends', 'App\Nova\Order_header'),
-            HasMany::make('รายการรับสินค้า', 'order_recs', 'App\Nova\Order_header'),
-
-        ];
-    }
-
-    /**
-     * Get the address fields for the resource.
-     *
-     * @return array
-     */
-    protected function contactFields()
-    {
-        return [
+            //new Panel('ข้อมูลการติดต่อ', $this->contactFields()),
             Text::make(__('Contact name'), 'contactname')
-                ->hideFromIndex(),
+                ->hideFromIndex()
+                ->canSee(function ($request) {
+                    return $request->user()->hasPermissionTo('manage order_headers');
+                }),
             Text::make(__('Email'), 'email')
-                ->hideFromIndex(),
+                ->hideFromIndex()
+                ->canSee(function ($request) {
+                    return $request->user()->hasPermissionTo('manage order_headers');
+                }),
             Text::make(__('Phone'), 'phoneno')
                 ->rules('required')
                 ->sortable()
                 ->hideFromIndex(),
             Text::make(__('Website Url'), 'weburl')
-                ->hideFromIndex(),
+                ->hideFromIndex()
+                ->canSee(function ($request) {
+                    return $request->user()->hasPermissionTo('manage order_headers');
+                }),
             Text::make(__('Facebook'), 'facebook')
-                ->hideFromIndex(),
+                ->hideFromIndex()
+                ->canSee(function ($request) {
+                    return $request->user()->hasPermissionTo('manage order_headers');
+                }),
             Text::make(__('Line'), 'line')
-                ->hideFromIndex(),
-
-        ];
-    }
-    /**
-     * Get the address fields for the resource.
-     *
-     * @return array
-     */
-    protected function addressFields()
-    {
-        return [
-
+                ->hideFromIndex()
+                ->canSee(function ($request) {
+                    return $request->user()->hasPermissionTo('manage order_headers');
+                }),
+            //new Panel('ที่อยู่ในการออกเอกสาร', $this->addressFields()),
             Text::make(__('Address'), 'address')
                 ->rules('required')
                 ->sortable()
@@ -218,27 +218,146 @@ class Customer extends Resource
                 ->rules('required')
                 ->hideFromIndex(),
             NovaGoogleMaps::make(__('Google Map Address'), 'location')->setValue($this->location_lat, $this->location_lng)
-                ->hideFromIndex(),
+                ->hideFromIndex()
+                ->canSee(function ($request) {
+                    return $request->user()->hasPermissionTo('manage order_headers');
+                }),
+            //new Panel('อื่นๆ', $this->otherFields()),
+            Image::make(__('Logo'), 'logofile')
+                ->hideFromIndex()
+                ->canSee(function ($request) {
+                    return $request->user()->hasPermissionTo('manage order_headers');
+                }),
+            Image::make(__('Image'), 'imagefile')
+                ->hideFromIndex()
+                ->canSee(function ($request) {
+                    return $request->user()->hasPermissionTo('manage order_headers');
+                }),
+            Textarea::make(__('Other'), 'description')
+                ->hideFromIndex()
+                ->canSee(function ($request) {
+                    return $request->user()->hasPermissionTo('manage order_headers');
+                }),
+            HasMany::make(__('Customer addresses'), 'addresses', 'App\Nova\Address'),
+            //BelongsToMany::make(__('Customer products'), 'product', 'App\Nova\Product'),
+            //HasMany::make(__('Customer shipping cost'), 'customer_product_prices', 'App\Nova\Customer_product_price'),
+            HasOne::make(__('Assign user'), 'assign_customer', 'App\Nova\User')
+                ->canSee(function ($request) {
+                    return $request->user()->role == 'admin';
+                }),
+            HasMany::make('รายการส่งสินค้า', 'order_sends', 'App\Nova\Order_header'),
+            HasMany::make('รายการรับสินค้า', 'order_recs', 'App\Nova\Order_header'),
 
         ];
     }
+
+    // /**
+    //  * Get the address fields for the resource.
+    //  *
+    //  * @return array
+    //  */
+    // protected function contactFields()
+    // {
+    //     return [
+    //         Text::make(__('Contact name'), 'contactname')
+    //             ->hideFromIndex()
+    //             ->canSee(function ($request) {
+    //                 return $request->user()->hasPermissionTo('manage order_headers');
+    //             }),
+    //         Text::make(__('Email'), 'email')
+    //             ->hideFromIndex()
+    //             ->canSee(function ($request) {
+    //                 return $request->user()->hasPermissionTo('manage order_headers');
+    //             }),
+    //         Text::make(__('Phone'), 'phoneno')
+    //             ->rules('required')
+    //             ->sortable()
+    //             ->hideFromIndex(),
+    //         Text::make(__('Website Url'), 'weburl')
+    //             ->hideFromIndex()
+    //             ->canSee(function ($request) {
+    //                 return $request->user()->hasPermissionTo('manage order_headers');
+    //             }),
+    //         Text::make(__('Facebook'), 'facebook')
+    //             ->hideFromIndex()
+    //             ->canSee(function ($request) {
+    //                 return $request->user()->hasPermissionTo('manage order_headers');
+    //             }),
+    //         Text::make(__('Line'), 'line')
+    //             ->hideFromIndex()
+    //             ->canSee(function ($request) {
+    //                 return $request->user()->hasPermissionTo('manage order_headers');
+    //             }),
+
+    //     ];
+    // }
     /**
      * Get the address fields for the resource.
      *
      * @return array
      */
-    protected function otherFields()
-    {
-        return [
-            Image::make(__('Logo'), 'logofile')
-                ->hideFromIndex(),
-            Image::make(__('Image'), 'imagefile')
-                ->hideFromIndex(),
-            Textarea::make(__('Other'), 'description')
-                ->hideFromIndex(),
+    // protected function addressFields()
+    // {
+    //     return [
 
-        ];
-    }
+    //         Text::make(__('Address'), 'address')
+    //             ->rules('required')
+    //             ->sortable()
+    //             ->hideFromIndex(),
+    //         InputSubDistrict::make(__('Sub District'), 'sub_district')
+    //             ->withValues(['district', 'amphoe', 'province', 'zipcode'])
+    //             ->fromValue('district')
+    //             ->sortable()
+    //             ->rules('required'),
+    //         InputDistrict::make(__('District'), 'district')
+    //             ->withValues(['district', 'amphoe', 'province', 'zipcode'])
+    //             ->fromValue('amphoe')
+    //             ->sortable()
+    //             ->rules('required'),
+    //         InputProvince::make(__('Province'), 'province')
+    //             ->withValues(['district', 'amphoe', 'province', 'zipcode'])
+    //             ->fromValue('province')
+    //             ->sortable()
+    //             ->rules('required'),
+    //         InputPostalCode::make(__('Postal Code'), 'postal_code')
+    //             ->withValues(['district', 'amphoe', 'province', 'zipcode'])
+    //             ->fromValue('zipcode')
+    //             ->rules('required')
+    //             ->hideFromIndex(),
+    //         NovaGoogleMaps::make(__('Google Map Address'), 'location')->setValue($this->location_lat, $this->location_lng)
+    //             ->hideFromIndex()
+    //             ->canSee(function ($request) {
+    //                 return $request->user()->hasPermissionTo('manage order_headers');
+    //             }),
+
+    //     ];
+    // }
+    /**
+     * Get the address fields for the resource.
+     *
+     * @return array
+     */
+    // protected function otherFields()
+    // {
+    //     return [
+    //         Image::make(__('Logo'), 'logofile')
+    //             ->hideFromIndex()
+    //             ->canSee(function ($request) {
+    //                 return $request->user()->hasPermissionTo('manage order_headers');
+    //             }),
+    //         Image::make(__('Image'), 'imagefile')
+    //             ->hideFromIndex()
+    //             ->canSee(function ($request) {
+    //                 return $request->user()->hasPermissionTo('manage order_headers');
+    //             }),
+    //         Textarea::make(__('Other'), 'description')
+    //             ->hideFromIndex()
+    //             ->canSee(function ($request) {
+    //                 return $request->user()->hasPermissionTo('manage order_headers');
+    //             }),
+
+    //     ];
+    // }
 
 
     /**
