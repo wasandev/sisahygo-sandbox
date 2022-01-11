e4c9f8f1977eb9543db88c8a271e02e307e1cf34
Wasan Ongart <o_wasan@hotmail.com>
2022-01-11 15:43:57 +0700

    update branch_balance

--- resources/views/reports/branchbalancereceipt.blade.php
+++ resources/views/reports/branchbalancereceipt.blade.php
@@ -136,11 +136,11 @@
                             {{ $item->customer->name}}
                         </td>
                         <td style="text-align: right">
-
+                            {{ $item->order_header->order_header_no}}
                         </td>
 
                         <td style="text-align: right">
-
+                            {{ $item->order_header->order_header_date}}
                         </td>
                         <td style="text-align: right">
                             {{ number_format($item->pay_amount,2,'.',',') }}
@@ -158,7 +158,7 @@
 
 
                     </tr>
-                    @foreach ($item->branch_balance_items as $balance_item)
+                    {{-- @foreach ($item->branch_balance_items as $balance_item)
                         <tr style="vertical-align: top;">
 
                             <td>
@@ -185,7 +185,7 @@
                             <td>
                             </td>
                         </tr>
-                    @endforeach
+                    @endforeach --}}
                 @endforeach
             @endforeach
         @endforeach
