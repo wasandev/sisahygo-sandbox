c086328706784e43ee5c144c6d1e2d987aba6fa3
Wasan Ongart <o_wasan@hotmail.com>
2022-01-14 14:39:29 +0700

    update permission

--- app/Policies/Productservice_pricePolicy.php
+++ app/Policies/Productservice_pricePolicy.php
@@ -16,9 +16,7 @@ class Productservice_pricePolicy
     }
     public function view(User $user, Productservice_price $productservice_price)
     {
-        if ($user->hasPermissionTo('view own productservice_prices')) {
-            return $user->id === $productservice_price->user_id;
-        }
+
 
         return $user->role == 'admin' || $user->hasPermissionTo('view productservice_prices');
     }
@@ -26,24 +24,20 @@ class Productservice_pricePolicy
 
     public function create(User $user)
     {
-        return $user->role == 'admin' || $user->hasAnyPermission(['manage productservice_prices', 'manage own productservice_prices']);
+        return $user->role == 'admin' || $user->hasAnyPermission(['create productservice_prices', 'manage own productservice_prices']);
     }
 
 
     public function update(User $user, Productservice_price $productservice_price)
     {
-        if ($user->hasPermissionTo('manage own productservice_prices')) {
-            return $user->id === $productservice_price->user_id;
-        }
-        return $user->role == 'admin' || $user->hasPermissionTo('manage productservice_prices');
+
+        return $user->role == 'admin' || $user->hasPermissionTo('edit productservice_prices');
     }
 
 
     public function delete(User $user, Productservice_price $productservice_price)
     {
-        if ($user->hasPermissionTo('manage own productservice_prices')) {
-            return $user->id === $productservice_price->user_id;
-        }
-        return $user->role == 'admin' || $user->hasPermissionTo('manage productservice_prices');
+
+        return $user->role == 'admin' || $user->hasPermissionTo('delete productservice_prices');
     }
 }
