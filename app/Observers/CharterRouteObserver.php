dabb9984b0c5bf1b1294330579a2efe2632fef34
Wasan Ongart <o_wasan@hotmail.com>
2022-01-15 15:04:18 +0700

    update charterroute

--- app/Observers/CharterRouteObserver.php
+++ app/Observers/CharterRouteObserver.php
@@ -11,8 +11,8 @@ class CharterRouteObserver
     {
         $from_district = Branch_area::find($charter_route->branch_area_id);
         $charter_route->name = $from_district->district . ' ' . $from_district->province . '-' . $charter_route->to_district . ' ' . $charter_route->to_province;
-        $distdata  = get_distance($from_district->district, $charter_route->to_district);
-        $charter_route->distance = $distdata['distance'];
+        //$distdata  = get_distance($from_district->district, $charter_route->to_district);
+        //$charter_route->distance = $distdata['distance'];
         $charter_route->user_id = auth()->user()->id;
     }
 
