fa87492591e0480a9f1e2bedfe54be372b374dd3
Wasan Ongart <o_wasan@hotmail.com>
2022-01-26 21:16:36 +0700

    update response

--- config/merq-nova-theme-responsive.php
+++ config/merq-nova-theme-responsive.php
@@ -3,7 +3,7 @@
 return [
 
     // If true, hide all sidebar headlines
-    'hide_all_sidebar_headlines' => true,
+    'hide_all_sidebar_headlines' => false,
 
     // List of sidebar headlines to hide (Ex.: ["Other"])
     'hidden_sidebar_headlines' => [],
@@ -12,7 +12,7 @@ return [
     'resource_tables_sticky_actions' => true,
 
     // If true, the resource tables actions will be always visible (sticky) on mobile
-    'resource_tables_sticky_actions_on_mobile' => false,
+    'resource_tables_sticky_actions_on_mobile' => true,
 
     // If true, hides the "Update & Continue Editing" button on "Update" forms
     'hide_update_and_continue_editing_button' => true,
