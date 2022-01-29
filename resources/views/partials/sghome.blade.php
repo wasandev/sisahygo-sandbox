c3e71dd7036ac2a3c92bfbab9a532f7740717b75
Wasan Ongart <o_wasan@hotmail.com>
2022-01-29 16:08:10 +0700

    update frontpage

--- resources/views/partials/sghome.blade.php
+++ resources/views/partials/sghome.blade.php
@@ -1,8 +1,5 @@
 <!--image home page-->
 
 <div class="max-w-full mx-auto  bg-gray-500 bg-cover items-center pt-16"
-    style="background-image:url('{{  Storage::url('sgcover.jpg') }}'); height: 70vh; ">
-
-
-
+    style="background-image:url('{{  Storage::url('images/sgcover.jpg') }}'); height: 70vh; ">
 </div>
--- resources/views/services/card.blade.php
+++ resources/views/services/card.blade.php
@@ -3,7 +3,7 @@
         <div>
                <a href="/tracking" class="flex flex-col flex-1 border border-gray-200  hover:shadow-xl hover:bg-blue-300 translateY-2px m-2 p-2 lg:m-4 lg:p-4 no-underline transition">
                 <div class="aspect-16x9"
-                   style="background:url('{{  Storage::url('tracking1.png') }}') no-repeat center center/cover">
+                   style="background:url('{{  Storage::url('images/tracking1.png') }}') no-repeat center center/cover">
                 </div>
 
                 <p class="text-base font-semibold text-center text-gray-800 p-2 ">ติดตามสถานะการขนส่งสินค้า</p>
@@ -13,7 +13,7 @@
         <div>
             <a href="/service_price" class="flex flex-col flex-1 border border-gray-200  hover:shadow-xl hover:bg-blue-300 translateY-2px m-2 p-2 lg:m-4 lg:p-4 no-underline transition">
                 <div class="aspect-16x9"
-                   style="background:url('{{  Storage::url('branch_area.jpg') }}') no-repeat center center/cover">
+                   style="background:url('{{  Storage::url('images/tracking1.png') }}') no-repeat center center/cover">
                 </div>
 
                 <p class="text-base font-semibold text-center text-gray-800 p-2 ">สอบถามพื้นที่บริการ</p>
@@ -23,7 +23,7 @@
         <div>
             <a href="/service_price" class="flex flex-col flex-1 border border-gray-200  hover:shadow-xl  hover:bg-blue-300 translateY-2px m-2 p-2 lg:m-4 lg:p-4 no-underline transition">
                 <div class="aspect-16x9"
-                   style="background:url('{{  Storage::url('tracking.jpg') }}') no-repeat center center/cover">
+                   style="background:url('{{  Storage::url('images/tracking1.png') }}') no-repeat center center/cover">
                 </div>
 
                 <p class="text-base font-semibold text-center text-gray-800 p-2 ">ราคาค่าขนส่งสินค้า</p>
