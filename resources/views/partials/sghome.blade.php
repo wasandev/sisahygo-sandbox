434e73cd1e8bf2bb72f9d32f48a5807616eea2c8
Wasan Ongart <o_wasan@hotmail.com>
2022-01-29 15:27:33 +0700

    update sccover

--- resources/views/partials/sghome.blade.php
+++ resources/views/partials/sghome.blade.php
@@ -1,7 +1,7 @@
 <!--image home page-->
 
 <div class="max-w-full mx-auto  bg-gray-500 bg-cover items-center pt-16"
-    style="background-image:url('{{  Storage::url('web/sgcover.jpg') }}'); height: 70vh; no-repeat center center/cover">
+    style="background-image:url('{{  Storage::url('web/sgcover.jpg') }}'); height: 70vh; ">
 
 
 
