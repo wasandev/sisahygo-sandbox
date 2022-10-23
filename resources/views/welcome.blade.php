a2307a16f7aeada6e51bf538746d823ed31416be
wasandev <o_wasan@hotmail.com>
2022-10-23 19:49:39 +0700

    test commit from docker

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -13,29 +13,31 @@
 @endsection
 
 @section('content')
-<div id="app" class="max-w-full " >
+    <div id="app" class="max-w-full ">
 
-    <div class="flex mx-4">
+        <div class="flex mx-4">
 
-        <div class="w-full mx-auto">
+            <div class="w-full mx-auto">
 
 
 
-                 <div class="p-4 m-4 max-w-full mx-auto text-center leading-tight bg-blue-500 rounded-lg shadow-md" >
+                <div class="p-4 m-4 max-w-full mx-auto text-center leading-tight bg-blue-500 rounded-lg shadow-md">
 
-                        <h1 class="mt-4 text-2xl text-gray-100 font-extrabold xl:text-xl leading-tight">
-                            SISAHY TRANSPORT
-                        </h1>
-                        <p class=" sm:block text-gray-100 text-2xl font-extrabold xl:text-xl ">
-                            บริษัท สี่สหายขนส่ง(1988) จำกัด
-                        </p>
-                        <p class="mt-2 leading-relaxed text-xl font-extrabold  text-gray-100  antialiased ">
-                            บริการรับ-ส่งสินค้า
-                        </p>
-                        <p></p>
-                        <h1 class="mb-4 text-2xl text-gray-100 font-extrabold xl:text-2xl leading-tight">
-                            CALL : 02 096 2444
-                        </h1>
+                    <h1 class="mt-4 text-2xl text-gray-100 font-extrabold xl:text-xl leading-tight">
+                        SISAHY TRANSPORT
+                    </h1>
+                    <p class=" sm:block text-gray-100 text-2xl font-extrabold xl:text-xl ">
+                        บริษัท สี่สหายขนส่ง(1988) จำกัด
+                    </p>
+                    <p class="mt-2 leading-relaxed text-xl font-extrabold  text-gray-100  antialiased ">
+                        บริการรับ-ส่งสินค้า
+                        ไปจังหวัดภาคใต้
+
+                    </p>
+                    <p></p>
+                    <h1 class="mb-4 text-2xl text-gray-100 font-extrabold xl:text-2xl leading-tight">
+                        CALL : 02 096 2444
+                    </h1>
 
                 </div>
                 <div class="py-16">
@@ -44,17 +46,15 @@
                 </div>
 
 
+            </div>
+
+
         </div>
 
 
     </div>
-
-
-</div>
 @endsection
 
 @section('footer')
     @include('partials.footer')
-
 @endsection
-
